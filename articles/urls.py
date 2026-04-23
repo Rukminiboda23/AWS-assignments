aff96d90552dc584e3ebb014a7080b5754ac9d7e
Rukminiboda23 <bodarukmini@gmail.com>
2026-04-23 18:56:07 +0530

    removed duplicate root files

--- articles/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.article_list, name='list'),
-    path('article/<int:id>/', views.article_detail, name='detail'),
-]
